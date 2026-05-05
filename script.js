39af1e848ab2612de69d7943d7b977ceed1b3243
g4 da goat <defnotg4@gmail.com>
2026-05-04 22:08:47 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,40 +1,6 @@
-const usersUrl =
-  "https://github.com/g4dagoat/wholelotta4c/blob/main/users.txt"
+const usersUrl = "https://raw.githubusercontent.com/g4dagoat/wholelotta4c/main/users.txt"
 
-function createUserBox(user, index) {
-  const div = document.createElement("div")
-  div.className = "user-box";
-
-  div.innerHTML = `
-          <div class="copied" id="copied-${index}">copied user</div>
-
-          <div class="copy-icon" data-name="${user}" data-id="${index}">
-            <i class="fa-regular fa-copy"></i>
-          </div>
-
-          <div class="user-name">${user}</div>
-        `
-
-  return div
-}
-
-function setupCopy() {
-  document.querySelectorAll(".copy-icon").forEach((icon) => {
-    icon.addEventListener("click", () => {
-      const text = icon.getAttribute("data-name")
-      const id = icon.getAttribute("data-id")
-
-      navigator.clipboard.writeText(text)
-
-      const el = document.getElementById("copied-" + id)
-      el.classList.add("show");
-
-      setTimeout(() => {
-        el.classList.remove("show")
-      }, 1000);
-    })
-  })
-}
+const container = document.querySelector(".container")
 
 fetch(usersUrl)
   .then((res) => res.text())
@@ -44,21 +10,17 @@ fetch(usersUrl)
       .map((u) => u.trim())
       .filter(Boolean)
 
-    const container = document.querySelector(".container")
-
     users.forEach((user, i) => {
       const div = document.createElement("div")
       div.className = "user-box"
 
       div.innerHTML = `
-        <div class="copied" id="copied-${i}">copied user</div>
-
-        <div class="copy-icon" data-name="${user}" data-id="${i}">
-          <i class="fa-regular fa-copy"></i>
-        </div>
-
-        <div class="user-name">${user}</div>
-      `;
+              <div class="copied" id="copied-${i}">copied user</div>
+              <div class="copy-icon" data-name="${user}" data-id="${i}">
+                <i class="fa-regular fa-copy"></i>
+              </div>
+              <div class="user-name">${user}</div>
+            `;
 
       container.appendChild(div)
     })
@@ -74,12 +36,10 @@ fetch(usersUrl)
         el.classList.add("show")
 
         setTimeout(() => el.classList.remove("show"), 1000)
-      });
-    });
+      })
+    })
   })
-  .catch((err) => {
-    console.error("Failed to load users:", err)
-  })
+  .catch(() => {})
 
 const isMobile = window.innerWidth < 600
 
